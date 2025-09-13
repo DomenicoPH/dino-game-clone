305d0fc59d11e45261195747a24cd45b14b815da
Domenico Pagano <dpaganoh@gmail.com>
2025-09-13 11:20:15 -0500

    player setGravity + setCollideWorldBounds + setBodySize

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,6 +1,9 @@
-import Phaser from "phaser";
+//import Phaser from "phaser";
+import { SpriteWithDynamicBody } from "../types";
 
 class PlayScene extends Phaser.Scene{
+
+    player: SpriteWithDynamicBody;
 
     get gameHeight(){ return this.game.config.height as number };
     get gameWidth(){ return this.game.config.width as number };
@@ -12,18 +15,27 @@ class PlayScene extends Phaser.Scene{
     create(){
         this.createEnvironment();
         this.createPlayer();
+        this.registerPlayerControl();
     };
 
     createPlayer(){
-        this.physics.add.sprite(0, this.gameHeight, 'dino-idle')
-        .setOrigin(0, 1)
+        this.player = this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1)
+        this.player
+            .setGravityY(5000)
+            .setCollideWorldBounds(true)
+            .setBodySize(44, 92)
     }
 
     createEnvironment(){
-        this.add
-            .tileSprite(0, this.gameHeight, 88, 26, 'ground')
-            .setOrigin(0, 1)
+        this.add.tileSprite(0, this.gameHeight, 88, 26, 'ground').setOrigin(0, 1)
     };
+
+    registerPlayerControl(){
+        const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        spaceBar.on('down', () => {
+            this.player.setVelocityY(-1600)
+        })
+    }
 
 };
 
