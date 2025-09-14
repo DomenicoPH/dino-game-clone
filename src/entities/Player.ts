841c64883e03f60b801be88e47d12e483e61a3da
Domenico Pagano <dpaganoh@gmail.com>
2025-09-13 20:38:28 -0500

    play dino-run animation

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -17,6 +17,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
             .setGravityY(5000)
             .setCollideWorldBounds(true)
             .setBodySize(44, 92)
+            this.registerAnimations();
     }
 
     update(){
@@ -26,5 +27,17 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         if(isSpaceJustDown && onFloor){
             this.setVelocityY(-1600);
         }
+    }
+
+    playRunAnimation(){
+        this.play('dino-run', true);
+    }
+    registerAnimations(){
+        this.anims.create({
+            key: 'dino-run',
+            frames: this.anims.generateFrameNames('dino-run', { start: 2, end: 3 }),
+            frameRate: 10,
+            repeat: -1,
+        })
     }
 }
