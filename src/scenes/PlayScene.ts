a09ffcb55eda278e752631a09121c4e62f52f2a3
Domenico Pagano <dpaganoh@gmail.com>
2025-09-13 20:09:21 -0500

    roll out the ground

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -5,7 +5,8 @@ import { SpriteWithDynamicBody } from "../types";
 class PlayScene extends Phaser.Scene{
 
     player: Player;
-    startTrigger: any;
+    ground: Phaser.GameObjects.TileSprite;
+    startTrigger: SpriteWithDynamicBody;
 
     get gameHeight(){ return this.game.config.height as number };
     get gameWidth(){ return this.game.config.width as number };
@@ -19,20 +20,31 @@ class PlayScene extends Phaser.Scene{
         this.createPlayer();
         this.startTrigger = this.physics.add.sprite(0, 10, null).setOrigin(0, 1).setAlpha(0);
         this.physics.add.overlap(this.startTrigger, this.player, () => {
-           if(this.startTrigger.y === 10){
-            this.startTrigger.body.reset(0, this.gameHeight);
-            return;
-           };
-           this.startTrigger.body.reset(9999, 9999);
+            if(this.startTrigger.y === 10){
+             this.startTrigger.body.reset(0, this.gameHeight);
+             return;
+            };
+            this.startTrigger.body.reset(9999, 9999);
+            this.time.addEvent({
+                delay: 1000 / 60,
+                loop: true,
+                callback: () => {
+                    if(this.ground.width <= this.gameWidth) this.ground.width += (17 * 2);
+                },
+            })
         })
     };
+
+    update(time: number, delta: number): void {
+
+    }
 
     createPlayer(){
         this.player = new Player(this, 0, this.gameHeight, 'dino-idle');
     }
 
     createEnvironment(){
-        this.add.tileSprite(0, this.gameHeight, 88, 26, 'ground').setOrigin(0, 1)
+        this.ground = this.add.tileSprite(0, this.gameHeight, 88, 26, 'ground').setOrigin(0, 1)
     };
 
 };
